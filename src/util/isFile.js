de06ce938fc4ec254e67c2196b362d19b3957576
Pablo Carreira <pablog.carreira@gmail.com>
2026-05-27 17:41:38 -0300

    fix: guard fs.existsSync against non-string values (Node 22 DEP0187)

--- src/util/isFile.js
+++ src/util/isFile.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 export default function isFile(val) {
-  if (fs.existsSync(val) && fs.statSync(val).isFile()) {
+  if (typeof val === 'string' && fs.existsSync(val) && fs.statSync(val).isFile()) {
     return true;
   } else {
     return false;
